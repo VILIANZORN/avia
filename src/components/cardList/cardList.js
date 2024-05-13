b8ecd977c0216a6757b9f40a1af93bad6713031b
VILIAN <64931291+VILIANZORN@users.noreply.github.com>
2024-05-13 18:32:21 +0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/cardList/cardList.js

--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { Empty, Spin, Alert } from 'antd';
+import { Offline, Online } from 'react-detect-offline';
 
 import ShowMore from '../show-more/show-more';
 import { fetchSearchId, fetchTickets } from '../../store/cardSlice';
@@ -30,13 +31,13 @@ export default function CardList() {
   const sortedDate = filtredTickets.slice().sort((a, b) => a.segments[0].duration - b.segments[0].duration);
   const loadingBar = loading ? <Spin /> : null;
   let maxTickets = sortedPrice.slice(0, ticketValue);
-  
+
   const errorCatchingFetchTickets = (id) => {
     fetchTicketsDispatch(id)
       .unwrap()
       .catch(() => {
-        setErr(prev => prev + 1);
-        if (err < 3) { 
+        setErr((prev) => prev + 1);
+        if (err < 3) {
           errorCatchingFetchTickets(id);
         } else {
           setShowAlert(true);
@@ -49,7 +50,7 @@ export default function CardList() {
   }, []);
 
   useEffect(() => {
-    if (searchId !==null && filterActive.length !== 0) {
+    if (searchId !== null && filterActive.length !== 0) {
       errorCatchingFetchTickets(searchId);
     }
   }, [searchId, tickets]);
@@ -69,17 +70,24 @@ export default function CardList() {
   }
 
   return (
-    <div className={classes.cardList}>
-      {loadingBar}
-      {showAlert && <Alert message="Произошла ошибка при загрузке данных, обновите страницу" type="error" />}
-      {maxTickets.map((ticket) => (
-        <Card key={uuidv4()} {...ticket} />
-      ))}
-      {filtredTickets.length ? (
-        <ShowMore onClick={handleShowMore} />
-      ) : (
-        <Empty description="Рейсов, подходящих под заданные фильтры, не найдено" />
-      )}
-    </div>
+    <>
+      <Online>
+        <div className={classes.cardList}>
+          {loadingBar}
+          {showAlert && <Alert message="Произошла ошибка при загрузке данных, обновите страницу" type="error" />}
+          {maxTickets.map((ticket) => (
+            <Card key={uuidv4()} {...ticket} />
+          ))}
+          {filtredTickets.length ? (
+            <ShowMore onClick={handleShowMore} />
+          ) : (
+            <Empty description="Рейсов, подходящих под заданные фильтры, не найдено" />
+          )}
+        </div>
+      </Online>
+      <Offline>
+        <Alert message="Error" description="К сожалению ты не в сети(" type="error" />
+      </Offline>
+    </>
   );
 }
